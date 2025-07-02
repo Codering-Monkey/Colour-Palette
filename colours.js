5f2ba5e561a4cd837c759339bf27a9bacb4c6912
Codering-Monkey <gamer_monkey@outlook.com.au>
2025-07-02 14:11:49 +1000

    Create colours.js

---
+++ colours.js
@@ -0,0 +1,58 @@
+export const colour = [
+	{
+		"name" : "Forest",
+		"main" : "#C8D7D2",
+		"dark" : "#373234",
+		"light" : "#F4F2F0",
+		"contrast" : "#7D8F69"
+	},
+	{
+		"name" : "Seafoam",
+		"main" : "#CCBCBC",
+		"dark" : "#323039",
+		"light" : "#FFFFFF",
+		"contrast" : "#18C3F8"
+	},
+	{
+		"name" : "Candyfloss",
+		"main" : "#F4D2D2",
+		"dark" : "#F49FB6",
+		"light" : "#FAF8F0",
+		"contrast" : "#A6E0DE"
+	},
+	{
+		"name" : "Earthen",
+		"main" : "#815B5B",
+		"dark" : "#594545",
+		"light" : "#FFF8EA",
+		"contrast" : "#9E7676"
+	},
+	{
+		"name" : "Retro",
+		"main" : "#FABB5A",
+		"dark" : "#132F63",
+		"light" : "#F7F1EA",
+		"contrast" : "#F6554D"
+	},
+	{
+		"name" : "Sunset",
+		"main" : "#FFBE86",
+		"dark" : "#F95A37",
+		"light" : "#F4F3EB",
+		"contrast" : "#7DC9E7"
+	},
+	{
+		"name" : "Vibrant",
+		"main" : "#EE316B",
+		"dark" : "#842D72",
+		"light" : "#F0F7F1",
+		"contrast" : "#38D7E7"
+	},
+	{
+		"name" : "Beach",
+		"main" : "#FFD379",
+		"dark" : "#FD892D",
+		"light" : "#F0F7F1",
+		"contrast" : "#470D57"
+	}
+]
